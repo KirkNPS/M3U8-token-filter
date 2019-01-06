c50f99450cbf9d32704366c41abdd41393ffcbe0
Jimmy Spets <jimspe@sodermalmskyrkan.net>
2019-01-06 21:26:38 +0100

    Uppdate code

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+const fs = require('fs');
 const http = require('http');
 const port = process.env.port || 3000;
 
+// readFile();
 const server = http.createServer((req,res) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    requesturl = req.url;
-    res.end(`Hello World!${requesturl}`);
+    fs.readFile(('.'+req.url), 'utf8', function (err,data) {
+        if (err) res.statusCode = 403;
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        requesturl = req.url;
+        res.write (data)
+        res.end();
+    });
 });
 
 server.listen(port, () => {
     console.log(`Server running on http://localhost:${port}/`);
 });
+
+//function readFile(filename) {
+//    console.log(`Filename: ${filename}`)
+//    return fs.readFileSync(("."+filename), 'utf8');
+//}
